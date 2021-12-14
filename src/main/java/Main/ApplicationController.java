529a38711c9888dcdadb719722cde7c1b33c1216
HillBryan <hillbj1@appstate.edu>
2021-12-13 19:38:41 -0500

    Small printing update.

--- src/main/java/Main/ApplicationController.java
+++ src/main/java/Main/ApplicationController.java
@@ -57,7 +57,7 @@ public class ApplicationController {
                 }
             }
         }
-        System.out.println("Friends in Dota 2 On Load: " + this.inDotaClientSet);
+        System.out.println("Friends in Dota 2: " + this.inDotaClientSet);
     }
 
     /**
