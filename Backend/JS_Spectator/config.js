4bdaece110b79a98cbc8af43329a6453f9bca60e
Bryan Hill <hillbj1@appstate.edu>
2021-12-17 23:43:08 -0500

    Adds.

---
+++ Backend/JS_Spectator/config.js
@@ -0,0 +1,13 @@
+// Configure and rename to "config.js"
+// Create an empty file called "sentry" in same directory
+// Have steamguard code = "" to start
+// Fill in the steamguard code after receiving it
+// After successfully logging in, return steamguard code to ""
+var config = {};
+
+config.steam_name = "Revenge Bot";
+config.steam_user = "dotarevenge004";
+config.steam_pass = "Revenge750461004";
+config.steam_guard_code = "steamGuardCodeIfApplicable";
+
+module.exports = config;
