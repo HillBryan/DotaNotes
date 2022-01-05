9d08cbbc24f27da8b57dd4058b38e9ef207a5c6a
HillBryan <hillbj1@appstate.edu>
2022-01-05 07:07:27 -0500

    Refactoring file names.

---
+++ Backend/Steam Bot/src/main/java/Credentials/Config.java
@@ -0,0 +1,14 @@
+package Credentials;
+
+/**
+ * @Author Bryan Hill
+ */
+public class Config {
+
+    // Steam Bot logon credentials.
+    public static final String USERNAME = "ENTER_HERE";
+    public static final String PASSWORD = "ENTER_HERE";
+
+    // Dota 2 API key.
+    public static final String API_KEY = "ENTER_HERE";
+}
